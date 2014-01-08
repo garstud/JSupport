f110cff12d1f0bf21a93123c60fc28bd5d90a3fb
Marc STUDER <dev@garstud.com>
2014-01-08 18:13:58 +0100

    Update support.php

--- composants/1-compoFE/site/support.php
+++ composants/1-compoFE/site/support.php
@@ -4,8 +4,7 @@ defined('_JEXEC') or die('Restricted access');
  
 // Get an instance of the controller prefixed by Compo name
 $controller = JControllerLegacy::getInstance('Support');
-//$controller = JController::getInstance('Support'); //Fatal error: Class 'JController' not found. Besoin du jimport explicite !
- 
+
 // Perform the Request task
 $input = JFactory::getApplication()->input;
 $task = $input->get('task', 'cmd');
