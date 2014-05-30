8284eb6406221d7d8313508471a2e1fe50f2de56
Marc STUDER <dev@garstud.com>
2014-05-30 23:20:48 +0200

    init compo1 view

---
+++ composants/1-compoFE/site/views/tickets/tmpl/default.php
@@ -0,0 +1,24 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+JHtml::_('behavior.framework'); // javascript Joomla object for grid.sort !
+?>
+
+<h1>Les Tickets du service Support</h1>
+
+<?php echo $this->loadTemplate('items'); ?>
+
+<div class="pagination">
+	<p class="counter">
+		<?php echo $this->pagination->getPagesCounter(); ?> 
+	</p>
+
+	<?php echo $this->pagination->getPagesLinks(); ?>
+
+	<p class="counter">
+		<?php echo $this->pagination->getResultsCounter(); ?>
+	</p>
+</div>
+
+
+<?php //echo $this->pagination->getListFooter(); ?>
